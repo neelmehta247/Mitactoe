a1a7429f605cb4cc92e27a0abcdc630fdc324819
Neel Mehta <neelmehta247@gmail.com>
2015-11-28 15:26:13 +0530

    Added clickListeners to the buttons.

--- app/src/main/java/com/tikotapps/mitactoe/LaunchActivity.java
+++ app/src/main/java/com/tikotapps/mitactoe/LaunchActivity.java
@@ -1,16 +1,30 @@
 package com.tikotapps.mitactoe;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class LaunchActivity extends AppCompatActivity {
+public class LaunchActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        initComponents();
+    }
+
+    private void initComponents() {
+        (findViewById(R.id.button11)).setOnClickListener(this);
+        (findViewById(R.id.button12)).setOnClickListener(this);
+        (findViewById(R.id.button13)).setOnClickListener(this);
+        (findViewById(R.id.button21)).setOnClickListener(this);
+        (findViewById(R.id.button22)).setOnClickListener(this);
+        (findViewById(R.id.button23)).setOnClickListener(this);
+        (findViewById(R.id.button31)).setOnClickListener(this);
+        (findViewById(R.id.button32)).setOnClickListener(this);
+        (findViewById(R.id.button33)).setOnClickListener(this);
     }
 
     @Override
@@ -33,5 +47,29 @@ public class LaunchActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button11:
+                break;
+            case R.id.button12:
+                break;
+            case R.id.button13:
+                break;
+            case R.id.button21:
+                break;
+            case R.id.button22:
+                break;
+            case R.id.button23:
+                break;
+            case R.id.button31:
+                break;
+            case R.id.button32:
+                break;
+            case R.id.button33:
+                break;
+        }
     }
 }
