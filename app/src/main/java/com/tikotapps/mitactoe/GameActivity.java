4a9ae4ed8cfdfb1c6cd01f054b3952c12ea697fa
Neel Mehta <neelmehta247@gmail.com>
2015-12-02 08:41:04 +0530

    Initialised the board with borders.

--- app/src/main/java/com/tikotapps/mitactoe/GameActivity.java
+++ app/src/main/java/com/tikotapps/mitactoe/GameActivity.java
@@ -1,9 +1,13 @@
 package com.tikotapps.mitactoe;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.amulyakhare.textdrawable.TextDrawable;
 
 /**
  * Created by neel on 28/11/15 at 3:26 PM.
@@ -14,6 +18,24 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         initComponents();
+        resetButtons();
+    }
+
+    private void resetButtons() {
+        TextDrawable blankBox = TextDrawable.builder().beginConfig()
+                .withBorder(4)
+                .endConfig()
+                .buildRect("", Color.WHITE);
+
+        ((ImageView) findViewById(R.id.button11)).setImageDrawable(blankBox);
+        ((ImageView) findViewById(R.id.button12)).setImageDrawable(blankBox);
+        ((ImageView) findViewById(R.id.button13)).setImageDrawable(blankBox);
+        ((ImageView) findViewById(R.id.button21)).setImageDrawable(blankBox);
+        ((ImageView) findViewById(R.id.button22)).setImageDrawable(blankBox);
+        ((ImageView) findViewById(R.id.button23)).setImageDrawable(blankBox);
+        ((ImageView) findViewById(R.id.button31)).setImageDrawable(blankBox);
+        ((ImageView) findViewById(R.id.button32)).setImageDrawable(blankBox);
+        ((ImageView) findViewById(R.id.button33)).setImageDrawable(blankBox);
     }
 
     private void initComponents() {
