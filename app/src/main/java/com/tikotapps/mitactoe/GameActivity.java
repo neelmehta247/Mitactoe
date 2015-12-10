274e4e690577c4459356e341477b2d30710216ea
Neel Mehta <neelmehta247@gmail.com>
2015-12-11 00:32:30 +0530

    Added end game checker and UI to go with it.

--- app/src/main/java/com/tikotapps/mitactoe/GameActivity.java
+++ app/src/main/java/com/tikotapps/mitactoe/GameActivity.java
@@ -1,5 +1,8 @@
 package com.tikotapps.mitactoe;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -140,5 +143,88 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                 .buildRect("X", color);
 
         view.setBackground(drawable);
+
+        if (isGameOver()) {
+            String winString;
+            if (player1Turn) {
+                winString = "Player 1 Wins!";
+            } else {
+                winString = "Player 2 Wins!";
+            }
+            AlertDialog dialog = new AlertDialog.Builder(this).setTitle(winString)
+                    .setMessage("Do you want to play a new game?")
+                    .setPositiveButton("Yes!", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            startGame();
+                        }
+                    })
+                    .setNegativeButton("No!", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            finish();
+                            Intent intent = new Intent(getApplicationContext(), LaunchActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                        }
+                    })
+                    .setCancelable(false)
+                    .create();
+            dialog.show();
+        }
+    }
+
+    private boolean isGameOver() {
+        //Checking columns
+        for (int i = 0; i < 3; i++) {
+            int total = 0;
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == 1) {
+                    total++;
+                }
+            }
+            if (total == 3) {
+                return true;
+            }
+        }
+
+        //Checking rows
+        for (int i = 0; i < 3; i++) {
+            int total = 0;
+            for (int j = 0; j < 3; j++) {
+                if (board[j][i] == 1) {
+                    total++;
+                }
+            }
+            if (total == 3) {
+                return true;
+            }
+        }
+
+        //Checking one diagonal
+        int total = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if ((i == j) && board[j][i] == 1) {
+                    total++;
+                }
+            }
+        }
+        if (total == 3) {
+            return true;
+        }
+
+        //Checking the other diagonal
+        total = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if ((i + j == 2) && board[i][j] == 1) {
+                    total++;
+                }
+            }
+        }
+        return total == 3;
     }
 }
