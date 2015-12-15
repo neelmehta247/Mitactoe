60d82af6c31482c3cf2a71b9b75b856ad379d15d
Neel Mehta <neelmehta247@gmail.com>
2015-12-15 08:24:46 +0530

    UI Functionality for the main screen

---
+++ app/src/main/java/com/tikotapps/mitactoe/Difficulty.java
@@ -0,0 +1,8 @@
+package com.tikotapps.mitactoe;
+
+/**
+ * Created by neel on 15/12/15 at 1:00 AM.
+ */
+public enum Difficulty {
+    EASY, MEDIUM, HARD, NONE
+}
--- app/src/main/java/com/tikotapps/mitactoe/LaunchActivity.java
+++ app/src/main/java/com/tikotapps/mitactoe/LaunchActivity.java
@@ -5,15 +5,67 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ViewFlipper;
 
-public class LaunchActivity extends AppCompatActivity {
+public class LaunchActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        setOnClickListeners();
+    }
+
+    private void setOnClickListeners() {
+        findViewById(R.id.buttonOnePlayer).setOnClickListener(this);
+        findViewById(R.id.buttonTwoPlayer).setOnClickListener(this);
+        findViewById(R.id.buttonInstructions).setOnClickListener(this);
+        findViewById(R.id.buttonSubmit).setOnClickListener(this);
+        findViewById(R.id.buttonEasy).setOnClickListener(this);
+        findViewById(R.id.buttonMedium).setOnClickListener(this);
+        findViewById(R.id.buttonHard).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int viewFlipperPageNumber = 0;
         Intent gameIntent = new Intent(this, GameActivity.class);
-        startActivity(gameIntent);
+        switch (view.getId()) {
+            case R.id.buttonOnePlayer:
+                viewFlipperPageNumber = 1;
+                findViewById(R.id.playerName).requestFocusFromTouch();
+                break;
+            case R.id.buttonTwoPlayer:
+                viewFlipperPageNumber = 2;
+                findViewById(R.id.playerOneName).requestFocusFromTouch();
+                break;
+            case R.id.buttonInstructions:
+                break;
+            case R.id.buttonSubmit:
+                gameIntent.putExtra("player1", ((EditText) findViewById(R.id.playerOneName)).getText().toString().trim());
+                gameIntent.putExtra("player2", ((EditText) findViewById(R.id.playerTwoName)).getText().toString().trim());
+                gameIntent.putExtra("difficulty", Difficulty.NONE);
+                startActivity(gameIntent);
+                break;
+            case R.id.buttonEasy:
+                gameIntent.putExtra("player1", ((EditText) findViewById(R.id.playerName)).getText().toString().trim());
+                gameIntent.putExtra("difficulty", Difficulty.NONE);
+                startActivity(gameIntent);
+                break;
+            case R.id.buttonMedium:
+                gameIntent.putExtra("player1", ((EditText) findViewById(R.id.playerName)).getText().toString().trim());
+                gameIntent.putExtra("difficulty", Difficulty.MEDIUM);
+                startActivity(gameIntent);
+                break;
+            case R.id.buttonHard:
+                gameIntent.putExtra("player1", ((EditText) findViewById(R.id.playerName)).getText().toString().trim());
+                gameIntent.putExtra("difficulty", Difficulty.HARD);
+                startActivity(gameIntent);
+                break;
+        }
+        ((ViewFlipper) findViewById(R.id.mainViewFlipper)).setDisplayedChild(viewFlipperPageNumber);
     }
 
     @Override
