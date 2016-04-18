bcd10996c06449b9cb5fef9d8ba652d8bc549a9d
Neel Mehta <neelmehta247@gmail.com>
2016-04-18 18:30:29 +0530

    Added instructions

---
+++ app/src/main/java/com/tikotapps/mitactoe/InstructionsActivity.java
@@ -0,0 +1,16 @@
+package com.tikotapps.mitactoe;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by neel on 18/04/16 at 6:29 PM.
+ */
+public class InstructionsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.instructions);
+    }
+}
--- app/src/main/java/com/tikotapps/mitactoe/LaunchActivity.java
+++ app/src/main/java/com/tikotapps/mitactoe/LaunchActivity.java
@@ -3,8 +3,6 @@ package com.tikotapps.mitactoe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 public class LaunchActivity extends AppCompatActivity implements View.OnClickListener {
@@ -32,29 +30,8 @@ public class LaunchActivity extends AppCompatActivity implements View.OnClickLis
                 startActivity(new Intent(this, TwoPlayerActivity.class));
                 break;
             case R.id.buttonInstructions:
+                startActivity(new Intent(this, InstructionsActivity.class));
                 break;
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_launch, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
